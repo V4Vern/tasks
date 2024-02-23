0422439a423b549ce238c0fbd68d158760d1ebb0
V4vern <28131050+V4Vern@users.noreply.github.com>
2024-02-23 20:52:36 +0800

    Add notification message

--- src/main/java/se/edu/streamdemo/Main.java
+++ src/main/java/se/edu/streamdemo/Main.java
@@ -17,6 +17,7 @@ public class Main {
 
         System.out.println("Printing deadlines ...");
         printDeadlines(tasksData);
+        printDeadlinesUsingStreams(tasksData);
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
